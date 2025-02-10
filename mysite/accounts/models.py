08c40894e6f97afef841a4b26608c9802aa54ba5
Lucius Rawkins <enotik455@mail.ru>
2025-02-10 20:26:05 +0300

    For exam1

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    is_admin = models.BooleanField(default=False)  # Добавляем это поле
+
+    # Указываем уникальные related_name для groups и user_permissions
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name="customuser_set",  # Уникальное имя для обратной связи
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name="customuser_set",  # Уникальное имя для обратной связи
+        related_query_name="user",
+    )
